4a2d3a1643dc73d912f0c26f1e9b96a34ff84949
berryallen1996 <berryallen01996@gmail.com>
2018-09-08 14:27:46 +0530

    App Submit

---
+++ dummyCalendarApp/AppDelegate.swift
@@ -0,0 +1,89 @@
+//
+//  AppDelegate.swift
+//  dummyCalendarApp
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) {(accepted, error) in
+            if !accepted {
+                print("Notification access denied.")
+            }
+        }
+       
+        let action1 = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: [])
+        let category = UNNotificationCategory(identifier: "remindLaterCategory", actions: [action1], intentIdentifiers: [], options: [])
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+        
+       
+       
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func scheduleNotification(at date: Date , count : String, food : String) {
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents(in: .current, from: date)
+        let newComponents = DateComponents(calendar: calendar, timeZone: .current, month: components.month, day: components.day, hour: components.hour, minute: components.minute)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+        let content = UNMutableNotificationContent()
+        content.title = "Meal Reminder"
+        content.body = "Please take \(food)..!"
+        content.sound = UNNotificationSound.default()
+        content.categoryIdentifier = "remindLaterCategory"
+        
+       UNUserNotificationCenter.current().delegate = self
+        
+        let request = UNNotificationRequest(identifier: "textNotification\(count)", content: content, trigger: trigger)
+       // UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+
+    }
+}
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.actionIdentifier == "remindLater" {
+            let newDate = Date(timeInterval: 60, since: Date())
+           
+            let calendar = Calendar(identifier: .gregorian)
+            let newComponent = calendar.dateComponents(in: .current, from: newDate)
+            let min = newComponent.minute
+            scheduleNotification(at: newDate, count: "\(min! * 2)", food: "Please take your meal.")
+        }
+    }
+}
---
+++ dummyCalendarApp/ViewController.swift
@@ -0,0 +1,265 @@
+//
+//  ViewController.swift
+//  dummyCalendarApp
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var dict = NSMutableDictionary()
+    var weekDiet = NSMutableDictionary()
+    var dietData = NSMutableArray()
+    var timeArr : [String] = ["2018-09-07 19:54:00","2018-09-06 19:55:00"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.getSampleData() //Sample data URL for testing purpose.
+//        self.getJsonFromUrl() //Server URL.
+    }
+    
+    func getJsonFromUrl(){
+        let session = URLSession.shared
+        let url = URL(string: "https://naviadoctors.com/dummy/")!
+        let task = session.dataTask(with: url) { (data, _, _) -> Void in
+            if let data = data {
+                let response = try? JSONSerialization.jsonObject(with: data, options: [])
+                self.weekDiet = (((response as! NSDictionary).object(forKey: "week_diet_data") as! NSDictionary).mutableCopy() as! NSMutableDictionary)
+                self.assignReminder()
+            }
+        }
+        task.resume()
+    }
+    
+    func assignReminder(){
+        
+        var today = Date()
+        
+        for _ in 1...7{
+            
+            let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: today)
+            let date = DateFormatter()
+            date.locale = Locale(identifier: "en_US")
+            date.timeZone = TimeZone.current
+            date.dateFormat = "yyyy-MM-dd"
+            let stringDate : String = date.string(from: today)
+            let weekDayName = (self.getDayNameBy(stringDate: stringDate)).lowercased()
+            
+            if let x = (self.weekDiet).object(forKey: weekDayName){
+                
+                for i in 0..<(x as! NSArray).count{
+                    let temp = NSMutableDictionary()
+                    let food = (((x as! NSArray).object(at: i) as! NSDictionary).object(forKey: "food")!)
+                    let mealDateTime = "\(stringDate) \((((x as! NSArray).object(at: i) as! NSDictionary).object(forKey: "meal_time"))!):00"
+                    
+                    temp.setObject(food, forKey: "food" as NSCopying)
+                    temp.setObject(mealDateTime, forKey: "meal_time" as NSCopying)
+                    self.dietData.add(temp)
+
+                }
+            }
+            today = tomorrow!
+        }
+        self.toScheduleNotification()
+//        print(self.dietData)
+    }
+    
+    func toScheduleNotification(){
+        for i in 0..<self.dietData.count{
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "UTC")
+            dateFormatter.timeZone = TimeZone.current
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:SS"
+            
+            let meal_time = dateFormatter.date(from: ((self.dietData.object(at: i) as! NSDictionary).object(forKey: "meal_time") as! String))
+            let food = (self.dietData.object(at: i) as! NSDictionary).object(forKey: "food") as! String
+            let reminderTime = Calendar.current.date(byAdding: .minute, value: -5, to: meal_time!)
+            debugPrint("===reminder Time \(reminderTime)")
+            let delegate = UIApplication.shared.delegate as? AppDelegate
+            
+            delegate?.scheduleNotification(at: reminderTime! , count: "\(i)", food: food)
+        }
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func datePickerDateChanged(_ sender: UIDatePicker) {
+      
+        let selectedDate = sender.date
+        for i in 0..<timeArr.count{
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US")
+            dateFormatter.timeZone = TimeZone.current
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:SS"
+            
+            let dateString = dateFormatter.string(from: selectedDate)
+            debugPrint("===date \(dateString)")
+            let date = dateFormatter.date(from: timeArr[i])
+            debugPrint("===Selected date \(date)")
+            let delegate = UIApplication.shared.delegate as? AppDelegate
+            delegate?.scheduleNotification(at: date! , count: "\(i)", food: "self.foodName")
+        }
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func getSampleData(){
+        let jsonString = "{\"diet_duration\": 20, \"week_diet_data\": {\"saturday\": [{\"food\": \"scramblled eggs\", \"meal_time\": \"13:05\"}, {\"food\": \"Burrito bowls\", \"meal_time\": \"13:15\"}, {\"food\": \"Evening snacks\", \"meal_time\": \"13:30\"}, {\"food\": \"North Indian thali\", \"meal_time\": \"13:50\"}], \"wednesday\": [{\"food\": \"Sprouts\", \"meal_time\": \"07:00\"}, {\"food\": \"Bread lintils and Rice\", \"meal_time\": \"16:00\"}, {\"food\": \"Soup ,Rice and Chicken\", \"meal_time\": \"21:00\"}], \"monday\": [{\"food\": \"Warm honey and water\", \"meal_time\": \"07:00\"}, {\"food\": \"proper thali\", \"meal_time\": \"15:00\"}]}}"
+    
+        let jsonData = jsonString.data(using: .utf8)
+        let dictionary = try? JSONSerialization.jsonObject(with: jsonData!, options: .mutableLeaves)
+        self.weekDiet = (((dictionary as! NSDictionary).object(forKey: "week_diet_data") as! NSDictionary).mutableCopy() as! NSMutableDictionary)
+        
+        self.assignReminder()
+    }
+    
+    //MARK: -----------TableView Delegate & Data Source Methods---------
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dietData.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell!
+        
+        cell = tableView.dequeueReusableCell(withIdentifier: "foodItemCell")
+
+        let results = self.dietData.object(at: indexPath.row) as! NSDictionary
+
+        let foodNameLabel = cell.viewWithTag(1) as! UILabel
+        let mealTimeLbl = cell.viewWithTag(2) as! UILabel
+        let borderLbl = cell.viewWithTag(3) as! UILabel
+        let imageView = cell.viewWithTag(4) as! UIImageView
+        let bgView = cell.viewWithTag(5)
+        
+        let mealDateTime = self.getDateFromString(dateString: "\(results.object(forKey: "meal_time")!)", fromDateString: "yyyy-MM-dd HH:mm:SS")
+        let currentDate = self.getDateFromString(dateString: self.getCurrentdate(), fromDateString: "yyyy-MM-dd HH:mm:SS")
+        let timeDiff = self.timeDifferenceInMinutes(firstDate: currentDate, secondDate: mealDateTime)
+        if  Int(timeDiff)! < 1{
+            borderLbl.isHidden = false
+            bgView?.backgroundColor = self.colorWithHexString("e74c3c")
+            imageView.image = #imageLiteral(resourceName: "cross")
+        }else{
+            borderLbl.isHidden = true
+            bgView?.backgroundColor = self.colorWithHexString("05C2C0")
+            imageView.image = #imageLiteral(resourceName: "check")
+        }
+        
+        let mealDateTimeString = self.getDateStringFromDateString(date: "\(results.object(forKey: "meal_time")!)", fromDateString: "yyyy-MM-dd HH:mm:SS", toDateString: "dd MMM, yyyy hh:mm a")
+        
+        foodNameLabel.text = "\((results.object(forKey: "food") as! String).uppercased())"
+
+        mealTimeLbl.text = "\(mealDateTimeString)"
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    //MARK: -----------Date Formatter Methods---------
+    
+    func getCurrentdate()->String{
+        let today = Date()
+        let date = DateFormatter()
+        date.locale = Locale(identifier: "en_US")
+        date.timeZone = TimeZone.current
+        date.dateFormat = "yyyy-MM-dd HH:mm:SS"
+        let stringDate : String = date.string(from: today)
+        return stringDate
+    }
+    
+    func getDateStringFromDateString(date: String, fromDateString: String, toDateString: String) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = fromDateString
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US") as Locale
+        let getDate = dateFormatter.date(from: date)
+        dateFormatter.dateFormat = toDateString
+        return dateFormatter.string(from: getDate!)
+    }
+    
+    func timeDifferenceInMinutes(firstDate:Date,secondDate:Date) -> String
+    {
+        
+        let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "en_US")
+        
+        dateformatter.dateFormat = "yyyy-MM-dd HH:mm:SS"
+        
+        let datecomponents = NSCalendar.current.dateComponents([.minute], from: firstDate, to: secondDate)
+        
+        let minute = datecomponents.minute!
+        
+        return "\(minute)"
+    }
+    
+    func getDateFromString(dateString:String,fromDateString:String) -> Date{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = fromDateString
+        let date = dateFormatter.date(from:dateString)!
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day, .hour], from: date)
+        let finalDate = calendar.date(from:components)
+        return finalDate!
+    }
+    
+    func getDateStringFromDate(date: Date, toDateString: String) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US") as Locale
+        dateFormatter.dateFormat = toDateString
+        return dateFormatter.string(from: date)
+    }
+    
+    func getDayNameBy(stringDate: String) -> String
+    {
+        let df  = DateFormatter()
+        df.dateFormat = "YYYY-MM-dd"
+        let date = df.date(from: stringDate)!
+        df.dateFormat = "EEEE"
+        return df.string(from: date);
+    }
+
+    
+    // MARK: - For Color code
+    func colorWithHexString (_ hex:String) -> UIColor
+    {
+        var cString:String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substring(from: 1)
+        }
+        
+        if (cString.count != 6) {
+            return UIColor.gray
+        }
+        
+        let rString = (cString as NSString).substring(to: 2)
+        let gString = ((cString as NSString).substring(from: 2) as NSString).substring(to: 2)
+        let bString = ((cString as NSString).substring(from: 4) as NSString).substring(to: 2)
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+        Scanner(string: rString).scanHexInt32(&r)
+        Scanner(string: gString).scanHexInt32(&g)
+        Scanner(string: bString).scanHexInt32(&b)
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
+    }
+    
+}
+
+
+
